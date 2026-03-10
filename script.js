22a296f19b8c8133f4882a09414170158b9ebe23
rawatanuj532-crypto <rawatanuj532@gmail.com>
2026-03-10 22:18:25 +0530

    Add custom cursor practice project

---
+++ script.js
@@ -0,0 +1,8 @@
+var crsr = document.querySelector('.cursor');
+var main = document.querySelector('#main');
+
+main.addEventListener('mousemove', function (dets) {
+
+    crsr.style.left = dets.x + "px";
+    crsr.style.top = dets.y + "px";
+})
